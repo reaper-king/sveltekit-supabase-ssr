5440dcbfa7e38f821e0b20a7f0a5f9283652437d
Jason Creviston <jason@malynium.com>
2023-11-13 17:09:48 -0500

    chore: tatu

--- src/routes/(authenticated)/admin/+page.server.ts
+++ src/routes/(authenticated)/admin/+page.server.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit'
 
 export const load = async ({ locals: { getSession }, depends }) => {
   depends('supabase:auth')
-  console.log('rerunning admin page server')
+
   const session = await getSession()
 
   if (!session) throw redirect(307, '/auth')
